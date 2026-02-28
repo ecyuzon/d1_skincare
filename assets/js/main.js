f6fe381722a206cda9789863c65fb9771aa3ce2f
Eljay Yuzon <ecyuzon@gmail.com>
2026-02-28 09:29:43 +0800

    Add Flickity carousel to testimonial and video reviews

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.body.classList.add('ready');
+
+  // Initialize Flickity carousel
+  var elem = document.querySelector('.main-carousel');
+  var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    wrapAround: true,
+    autoPlay: 3000,
+    pauseAutoPlayOnHover: false,
+    pageDots: false,
+    
+  });
+
+  var videoCarouselElem = document.querySelector('.video-carousel');
+  var videoFlkty = new Flickity(videoCarouselElem, {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    wrapAround: true,
+    autoPlay: 3000,
+    pauseAutoPlayOnHover: false,
+    pageDots: false,  
+  });
+
+
 });
